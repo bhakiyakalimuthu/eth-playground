5e69fb2068f2a6b5409e1bd981a08566f5ea670e
Bhakiyaraj Kalimuthu <bhakiya.kalimuthu@gmail.com>
2022-02-02 09:29:43 +0100

    add client

---
+++ client.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	client, err := ethclient.Dial("https://mainnet.infura.io")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
